c13f736c8ce9216ad2316bf699fc2cfede21a71c
Kyle Burton <kyle.burton@gmail.com>
2016-10-28 17:48:21 -0700

    formatting and split the megahertz

--- examples/golang/main.go
+++ examples/golang/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  fmt.Println("Hello Worl")
+	fmt.Println("Hello Worl")
 }
 
 var firstNames = []string{"first"}
 var lastNames = []string{"last"}
 
-func GetRandNames(numNames int) []string{
-  return GetRandNamesImpl(numNames,firstNames, lastNames)
+func GetRandNames(numNames int) []string {
+	return GetRandNamesImpl(numNames, firstNames, lastNames)
 }
 
-func GetRandNamesImpl(numNames int, firstNames []string, lastNames []string) []string{
-  return make([]string,numNames)
+func GetRandNamesImpl(numNames int, firstNames []string, lastNames []string) []string {
+	return make([]string, numNames)
 }
