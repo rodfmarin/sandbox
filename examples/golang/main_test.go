f3d480dea93ef778bcc34364a54c609d4890b284
Rodrigo Marin <rmarin@riotgames.com>
2017-01-03 18:09:50 -0800

    functional go and tests!

--- examples/golang/main_test.go
+++ examples/golang/main_test.go
@@ -24,5 +24,29 @@ func TestGetRandNames(t *testing.T) {
 	if res[0] != "solid snake" {
 		t.Error("Expected a name array containing 'solid snake', got %v", res[0])
 	}
+}
+
+func TestGetRandName(t *testing.T) {
+  res := GetRandName([]string{"solid"}, []string{"snake"})
+	if res != "solid snake" {
+		t.Error("Expected a name array containing 'solid snake', got %v", res)
+	}
+}
+
+func TestGetRandNamePure(t *testing.T) {
+	res := GetRandNamePure([]string{"A","B"}, []string{"1","2"},
+		func(n int) int{
+			return 0
+		})
+		if res != "A 1" {
+			t.Error("Expected a name array containing 'A 1', got %v", res)
+		}
+	res = GetRandNamePure([]string{"A","B"}, []string{"1","2"},
+		func(n int) int{
+			return 1
+		})
+		if res != "B 2" {
+			t.Error("Expected a name array containing 'B 2', got %v", res)
+		}
 
 }
